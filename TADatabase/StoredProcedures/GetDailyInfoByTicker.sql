fefcb95d8b05a7feb6afb0b9d51f39948aa3af97
YoureaBosonHarry <rath3.14159@protonmail.com>
2020-12-28 22:43:09 -0600

    Added function to get daily info

---
+++ TADatabase/StoredProcedures/GetDailyInfoByTicker.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION GetDailyInfoByTicker(_Ticker VARCHAR(6)) 
+RETURNS TABLE (
+    Id Int,
+    Ticker VARCHAR(6),
+	DateAdded DATE,
+    DailyLow NUMERIC(10, 2),
+    DailyHigh NUMERIC(10, 2),
+    DailyOpen NUMERIC(10, 2),
+    DailyClose NUMERIC(10, 2),
+    Volume INT,
+    AdjClose NUMERIC(10, 2))
+LANGUAGE plpgsql
+AS $$
+BEGIN 
+RETURN QUERY SELECT * FROM DailyInfo WHERE DailyInfo.Ticker = _Ticker;
+END;
+$$;
